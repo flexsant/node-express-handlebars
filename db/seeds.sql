744e0f104bdb096a58dd1929766653e79e32e3b1
flexsant <flexsant@gmail.com>
2019-10-09 19:13:58 -0700

    schema.sql, seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('impossible', false);
